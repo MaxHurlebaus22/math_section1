e0a620d3d79b1660172e242d13f3cb219ef8fddf
Maxwell Hurlebaus <hurlebmo@dukes.jmu.edu>
2016-04-29 17:19:50 -0400

    First commit

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -6,16 +6,19 @@ describe("A simple arithmetic program", function() {
 	it("can add two numbers", function() {
 		expect(math.add(3,5)).toBe(8);
 	});
-    //For subtraction
+    
     it("can subtract two numbers", function() {
     	expect(math.subtract(3,5)).toBe(-2);
     });
-    //For Multiplication
+    
 	it("can multiply two numbers", function() {
 		expect(math.multiply(3,5)).toBe(15);
 	});
-	//For Division
+	
 	it("can divide two numbers", function() {
 		expect(math.divide(15,5)).toBe(3);
+		expect(math.divide(15,0)).toBe("Error! Division by zero.");
 	});
+	
+});
 
